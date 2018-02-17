368b8dd85b3fff7605afc423ebc4ce6ba241f8ab
Harleigh Abel <harleighabel@gmail.com>
2018-02-17 07:01:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,15 +8,6 @@ class Anagram
   end
   
   def match(word_string)
-    anagram_array = []
-    word_string.each do |anagram|
-    
-      if anagram.split("").sort == word.split("").sort
-        anagram_array << anagram
-    
-      end
-    end 
-    anagram_array
-  end
-    
+    word_string.select {|anagram| anagram.split("").sort == word.split("").sort}
+  
 end  
